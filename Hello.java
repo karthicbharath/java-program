749bbcbe42f6e8f371c2a2303f85dec71b200306
karthicbharath <karthicbharath222@gmail.com>
2017-07-04 16:53:15 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Hello{
+public static void main(String args[])throw IOException{
+for(int i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
