a998772dd96507fd7fcab4569980f554afbc1e00
karthicbharath <karthicbharath222@gmail.com>
2017-07-05 15:19:32 +0530

    Update Amstrong.java

--- Amstrong.java
+++ Amstrong.java
@@ -1,20 +1,22 @@
 import java.io.*;
 import java.util.*;
 public class Amstrong{
-public static void main(String args[]){
-Scanner s=new Scanner(System.in);
-int n=s.nextInt();
-while(n!=0)
-{
-int a=n%10;
-n=n/10;
-sum=sum+(r*r*r);
-}
-if(sum==temp)
-{
-System.out.println("Amstrong Number");
-}
-else
-System.out.println("not Amstrong number");
-}
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int sum=0,temp;
+    int n=s.nextInt();
+    temp=n;
+    while(n!=0)
+    {
+      int a=n%10;
+      n=n/10;
+      sum=sum+(a*a*a);
+    }
+    if(sum==temp)
+    {
+      System.out.println("Amstrong Number");
+    }
+    else
+    System.out.println("not Amstrong number");
+  }
 }
