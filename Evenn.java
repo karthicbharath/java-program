94b8078509f3768e1aff02af7989b1320058f3de
karthicbharath <karthicbharath222@gmail.com>
2017-07-05 14:31:46 +0530

    Update and rename Even.java to Evenn.java

---
+++ Evenn.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Evenn{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int a=s.nextInt();
+    for(int i=n+1;i<a;i++)
+    {
+      if(i%2==0)
+      {
+        System.out.println(i);
+      }
+    }
+  }
+}
