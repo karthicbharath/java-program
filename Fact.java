716fea69090e2d47d71b4e35d5b1d28acc727db4
karthicbharath <karthicbharath222@gmail.com>
2017-07-05 09:20:41 +0530

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -5,9 +5,10 @@ public static void main(String args[]){
 Scanner s=new Scanner(System.in);
 int f=1;
 int n=s.nextInt();
-for(int i=0;i<n;i++)
+for(int i=1;i<=n;i++)
 {
 f=f*i;
 }
+  System.out.println(f);
 }
 }
